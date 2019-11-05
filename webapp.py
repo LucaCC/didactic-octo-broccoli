af6ad040db9618d704089c476efd3229ae98271e
Luca <lucaccorey@gmail.com>
2019-11-05 09:57:13 -0800

    Drop Down with States (Step 9)

--- webapp.py
+++ webapp.py
@@ -6,13 +6,46 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    return render_template('Home.html', states = get_state_options(counties))
 
-def get_state_options():
-    states = []
+def state_fun_fact(state, counties):
+    state = {}
+    state_fun_fact = ""
     for data in counties:
-        if data["State"] != states[data["State"]]:
-            states = data["State"]
+        if data["State"] in state:
+            state[data["State"]]["Bach"] += data["Education"]["Bachelor's Degree or Higher"]
+            state[data["State"]]["High"] += data["Education"]["High School or Higher"]
+            state[data["State"]]["Count"] += 1
+        else:
+            state[data["State"]] = {}
+            state[data["State"]]["Bach"] = data["Education"]["Bachelor's Degree or Higher"]
+            state[data["State"]]["High"] = data["Education"]["High School or Higher"]
+            state[data["State"]]["Count"] = 1
+        state_fun_fact = data["State"]
+
+    for data in state:
+        state[data]["Bach"] = (state[data]["Bach"]/state[data]["Count"])*2 # Weighted because higher degree
+        state[data]["High"] = (state[data]["High"]/state[data]["Count"])
+        state[data]["Edu"] = state[data]["Bach"] + state[data]["High"]
+
+    # for data in state:
+    #     if state[data]["Edu"] > state[state_fun_fact]["Edu"]:
+    #         state_fun_fact = data
+
+    return state_fun_fact
+
+def get_state_options(counties):
+    states = []
+    print("Render")
+    for data in counties:
+        if data["State"] not in states:
+            states.append(data["State"])
+    options = ""
+    for data in states:
+        options = options + Markup("<option value=\"" + data + "\">" + data + "</option>")
+    return options
 
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+    app.run(debug=True)
